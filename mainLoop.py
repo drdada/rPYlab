64efe58ad5f9e8572c0507388a6612b75ec4fb44
Drdada <drdada@hotmail.com>
2015-12-20 23:57:19 +0100

    Début de programmation avec les threads, Non fonctionnel !

--- mainLoop.py
+++ mainLoop.py
@@ -1,22 +1,60 @@
+# -*- coding: utf-8 -*-
+
 import RPi.GPIO as GPIO
 import time
 import pingLib
 import blinkLib
+import logging
+from threading import Thread
+logging.basicConfig(level=logging.DEBUG,
+        format='%(asctime)s %(levelname)-8s %(message)s',
+        datefmt='%a, %d %b %Y %H:%M:%S',
+        filename='/var/log/mainLoop.log')
 
 sleeptime=10 #in sec
+hostPing ="192.168.1.1" # IP host to check
 
-def loop():
-	resPing= pingLib.check_ping("192.168.1.1")
-	if resPing == "1":
-		blinkLib.blink(2,2)
-	time.sleep(sleeptime)
+global ping1_flag
 
+def pingTest():
+	while True:
+		try:
+			resPing= pingLib.check_ping(hostPing)
+		except:
+			print("Ping command Failed !")
+			logging.error('Ping command Failed !')
+		if resPing == "1": #1=ok 0=Failed
+			print("Ping ALERT")
+			logging.info('Host %s is not reachable',hostPing)
+		ping1_flag = resPing
+		time.sleep(sleeptime)
+		print("On a attendu, on va recommencer")
+	
+def ledNotification():
+	while True:
+		print("On va vérifier les flag")
+		if ping1_flag == "1":
+			print("J'ai recu un flag, on clignote")
+			blinkLib.blink(2,2)
+		time.sleep(5)
 
-#At the end
+#Da Main loop
 if __name__ == '__main__':
 	try:
 		print 'Press Ctrl-C to quit.'
-		while True:
-			loop()
+		thread_pingTest = pingTest()
+		thread_ledNotif = ledNotification()
+		
+		thread_pingTest.start()
+		print 'Thread pingstart ok'
+		thread_ledNotif.start()
+		print 'Thread ledNotif ok'
+
+
+		thread_pingTest.join()
+                thread_ledNotif.join()
+
+	except KeyboardInterrupt:
+		print("Bye")
 	finally:
 		GPIO.cleanup()
--- pingLib.py
+++ pingLib.py
@@ -1,8 +1,4 @@
 import logging,os
-logging.basicConfig(level=logging.DEBUG,
-	format='%(asctime)s %(levelname)-8s %(message)s',
-	datefmt='%a, %d %b %Y %H:%M:%S',
-	filename='/var/log/pingTest.log')
 
 def check_ping(hostname):
 	response = os.system("ping -c 1 " + hostname)
@@ -13,6 +9,5 @@ def check_ping(hostname):
 	else:
 		#Non-OK
 		pingstatus = "0"
-		logging.info('Host %s is not reachable',hostname)	
 
 	return pingstatus
