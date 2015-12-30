623884b277e05d035533122bf0bb992ee5a684de
Drdada <drdada@hotmail.com>
2015-12-30 11:55:28 +0100

    Mail check fully working

--- mainLoop.py
+++ mainLoop.py
@@ -6,16 +6,29 @@ import pingLib
 import blinkLib
 import logging
 import threading
+import ConfigParser
+
+## Log Config
 logging.basicConfig(level=logging.DEBUG, #Change it for different log level (debug, info)
         format='%(asctime)s :: %(levelname)-8s :: %(message)s',
         datefmt='%a, %d %b %Y %H:%M:%S',
         filename='/var/log/mainLoop.log')
 
-sleeptime=10 #in sec
+## Mail Config (see also mail.conf)
+mailConfig = ConfigParser.ConfigParser()
+mailConfig.readfp(open(r'mail.conf'))
+userMail = mailConfig.get('mailSettings', 'user')
+pwMail = mailConfig.get('mailSettings', 'password')
+
+## Check Ping COnfig
+pingSleeptime=10 #in sec for the check ping interval
 hostPing ="192.168.1.1" # IP host to check
+
+### END CONFIG
 
 global ping1_flag
 ping1_flag = 0
+global gmail1_flag
 global threadOn #Flag to kill or not the threads
 threadOn = False
 logging.info("Starting Python Script Monitoring daemon")
@@ -31,20 +44,57 @@ def pingTest():
 			logging.error('Ping command Failed !')
 		if resPing == "0": #1=ok 0=Failed
 			logging.info('Host %s is not reachable',hostPing)
+		else:
+			logging.debug('Host %s : OK',hostPing)
 		ping1_flag = resPing
-		time.sleep(sleeptime)
+		time.sleep(pingSleeptime)
 	logging.debug("Exiting PingTest Thread")
 	
 def ledNotification():
-	global ping1_flag, threadOn
+	global ping1_flag, gmail1_flag, threadOn
 	logging.debug("Starting LedNotif Thread")
+	time.sleep(6)
 	while not threadOn:
-		print(ping1_flag)
 		if ping1_flag == "0":
 			logging.debug("Ping Flag received: Blinking !")
-			blinkLib.blink(2,2)
-		time.sleep(5)
+			blinkLib.blink(19,float(0.1))
+		if ping1_flag == "1" and gmail1_flag > 0:
+			logging.debug("Gmail flag received: Blinking")
+			blinkLib.blink(gmail1_flag,2)
+		time.sleep(10)
 	logging.debug("Exiting PingTest Thread")
+
+#TY to https://gist.github.com/vadviktor/3529647#file-gmail-py 
+def gmail_connect(username,password):
+  import imaplib,re
+  i=imaplib.IMAP4_SSL('imap.gmail.com')
+  try:
+    i.login(username,password)
+    x,y=i.status('INBOX','(MESSAGES UNSEEN)')
+    messages=int(re.search('MESSAGES\s+(\d+)',y[0]).group(1))
+    unseen=int(re.search('UNSEEN\s+(\d+)',y[0]).group(1))-2
+    return (messages,unseen)
+  except:
+    return False,0
+
+def gmail_check():
+        global gmail1_flag, threadOn
+	time.sleep(2)
+        logging.debug("Starting Gmail_check Thread")
+        while not threadOn:
+                try:
+			messages,unread = gmail_connect(userMail,pwMail)
+                except:
+                        print("Gmail Check command Failed !")
+                        logging.error('Gmail check command Failed !')
+                if unread == 0: 
+                        logging.debug('Gmail Check: No new messages - Total: %i ',messages)
+		else:
+			logging.info('Gmail Check: %i new messages ! - Total: %i ',unread,messages)
+                gmail1_flag = unread
+                time.sleep(10)
+        logging.debug("Exiting gmailCheck Thread")
+
 
 #Da Main loop
 if __name__ == '__main__':
@@ -52,9 +102,12 @@ if __name__ == '__main__':
 		print("Press Ctrl-C to quit.")
 		thread_pingTest = threading.Thread(name='pingTest', target=pingTest)
 		thread_ledNotif = threading.Thread(name='ledNotification', target=ledNotification)
+		thread_gmailNotif = threading.Thread(name='gmail_check', target=gmail_check)
 		
 		thread_pingTest.start() #start threads
 		thread_ledNotif.start()
+		thread_gmailNotif.start()
+
 		while True:		#Loop in order to wait for the ctrl-c
 			time.sleep(1)        			
 	except KeyboardInterrupt:
